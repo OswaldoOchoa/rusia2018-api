c275e2390ac1cc1033d04c0e49f82df23000d15c
Elyager <elyager@gmail.com>
2018-05-22 16:06:54 -0700

    Split code in functions and refactor

--- playing-json.py
+++ playing-json.py
@@ -18,11 +18,10 @@ def write_json(filePath, data):
   with open(filePath, 'w') as file:
     json.dump(data, file, indent=2*" ")
 
-
 #Raffle
 def run_one_round():
   for i in range(1, 9):
-    choose_team(i)
+    start(i)
 
 def raffle_contenders():
   with open("users.json", "r") as file:
@@ -44,10 +43,7 @@ def valid_group(group, isFavorite):
   else:
     return True
 
-def choose_team(user_id):
-  data_users = read_json("users.json")
-  users_list = data_users["users"]
-
+def get_user_data(user_id, users_list):
   for user in users_list:
     if user["id"] == user_id:
       #Decide if choose a favorite team
@@ -62,39 +58,75 @@ def choose_team(user_id):
       else:
         isFavorite = choice([True, False])
 
-      # all open groups
-      data_groups = read_json("groups.json")
-      groups_list = [group for group in data_groups["groups"] if not group["closed"]]
+      return {
+        "user": user,
+        "favorite": isFavorite
+      }
 
-      # choose an open group untils is not previously chosen
-      group_chosen = choice(groups_list)
-      while(group_chosen["letter"] in user["groups"]
-            or not valid_group(group_chosen, isFavorite)):
-        group_chosen = choice(groups_list)
+def choose_group(user_data, groups_list):
+  user = user_data["user"]
+  isFavorite = user_data["favorite"]
+  open_groups = [group for group in groups_list if not group["closed"]]
 
-      teams_list = [team for team in group_chosen["teams"] if not team["picked"] and team["favorite"] == isFavorite]
+  if (len(open_groups) == 1):
+    return open_groups[0]
 
-      # choose an open group untils is not previously chosen
-      team_chosen = choice(teams_list)
+  group_chosen = choice(open_groups)
 
-      team_chosen["picked"] = True
+  while(
+    group_chosen["letter"] in user["groups"]
+    or not valid_group(group_chosen, isFavorite)
+  ):
+    group_chosen = choice(open_groups)
 
-      # check if the group needs to be closed
-      picked_teams = [team for team in group_chosen["teams"] if team["picked"]]
-      if len(picked_teams) == 4:
-        group_chosen["closed"] = True
+  return group_chosen
 
-      write_json("groups.json", data_groups)
+def choose_team(user_data, group):
+  isFavorite = user_data["favorite"]
 
-      user["groups"].append(group_chosen["letter"])
-      user["teams"].append({
-          "name": team_chosen["name"],
-          "favorite": team_chosen["favorite"]
-          })
-      write_json("users.json", data_users)
-      break
+  teams_list = [team for team in group["teams"] if not team["picked"] and team["favorite"] == isFavorite]
 
+  team_chosen = choice(teams_list)
+
+  return team_chosen
+
+def verify_group(group):
+    # check if the group needs to be closed
+  picked_teams = [team for team in group["teams"] if team["picked"]]
+
+  if len(picked_teams) == 4:
+    group["closed"] = True
+    return True
+
+def start(user_id):
+  try:
+    data_groups = read_json("groups.json")
+    groups_list = data_groups["groups"]
+
+    data_users = read_json("users.json")
+    users_list = data_users["users"]
+
+    user_data = get_user_data(user_id, users_list)
+    user = user_data["user"]
+
+    group_chosen = choose_group(user_data, groups_list)
+    user["groups"].append(group_chosen["letter"])
+
+    team_chosen = choose_team(user_data, group_chosen)
+    team_chosen["picked"] = True
+    user["teams"].append({
+      "name": team_chosen["name"],
+      "favorite": team_chosen["favorite"]
+    })
+
+    group_closed = verify_group(group_chosen)
+
+    write_json("groups.json", data_groups)
+    write_json("users.json", data_users)
+  except Exception as ex:
+    print(ex)
 
 if __name__ == "__main__":
+  free_teams = 32
   for i in range(4):
     run_one_round()
